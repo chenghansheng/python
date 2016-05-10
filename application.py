1175a0e724c0d044d7ac1578c9b52db1cbffca37
unknown <chenghansheng@gds-services.com>
2016-05-10 10:11:45 +0800

    Signed-off-by: unknown <chenghansheng@gds-services.com>

---
+++ application.py
@@ -0,0 +1,69 @@
+import os
+import flask
+import MySQLdb
+from flask import request
+
+application = flask.Flask(__name__)
+application.debug = True
+
+@application.route('/')
+def hello_world():
+  return "Hello World DaoCloud!"
+
+@application.route('/env')
+def env():
+  envs = "Environments: <br>"
+  for key in os.environ.keys():
+    envs = envs + "%s: %s<br>" % (key,os.environ[key])
+
+  return envs
+
+@application.route('/header')
+def header():
+  headers = "HTTP Headers: <br>"
+  for h in request.headers.keys():
+    headers = headers + "%s: %s<br>" % (h,request.headers.get(h))
+
+  return headers
+
+@application.route('/score')
+def score():
+  storage = Storage()
+  storage.populate()
+  score = storage.score()
+  return "Hello world, %d!" % score
+
+class Storage():
+  def __init__(self):
+    self.db = MySQLdb.connect(
+      user   = os.getenv('MYSQL_USERNAME'),
+      passwd = os.getenv('MYSQL_PASSWORD'),
+      db     = os.getenv('MYSQL_INSTANCE_NAME'),
+      host   = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
+      port   = int(os.getenv('MYSQL_PORT_3306_TCP_PORT'))
+    )
+
+    cur = self.db.cursor()
+    cur.execute("DROP TABLE IF EXISTS scores")
+    cur.execute("CREATE TABLE scores(score INT)")
+
+  def populate(self):
+    cur = self.db.cursor()
+    sql = "INSERT INTO scores(score) VALUES(1234)"
+
+    try:
+      # execute SQL inside a transaction
+      cur.execute(sql)
+      self.db.commit()
+    except:
+      # rollback if neccessary
+      self.db.rollback()
+
+  def score(self):
+    cur = self.db.cursor()
+    cur.execute("SELECT * FROM scores")
+    row = cur.fetchone()
+    return row[0]
+
+if __name__ == "__main__":
+  application.run(host='0.0.0.0', port=3000)
